cb1d415da6f7d4aae11db7394b1ba93c51f0b672
Agung Saputro <agung.saputro888@outlook.com>
2025-10-02 09:02:29 +0700

    additional update never miss a post

--- src/components/molecules/NeverMissAPost.tsx
+++ src/components/molecules/NeverMissAPost.tsx
@@ -8,17 +8,17 @@ const NeverMissAPost: React.FC = () => {
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
        
         <h2 className="text-3xl sm:text-4xl font-bold text-white mb-2 font-dancingScript">
-          Never Miss a Post
+          Tetap Terinspirasi Bersama Rajut Dyubi
         </h2>
         <p className="text-white max-w-2xl mx-auto mb-6 mt-6 text-[0.9em]">
-         Subscribe to our newsletter and get the latest tutorials, tips, and stories delivered straight to your inbox every week.
+          Berlangganan newsletter kami dan dapatkan update mingguan tentang produk rajut terbaru, tutorial kreatif, dan diskon eksklusif langsung ke inbox Anda.
         </p>
         <div className="mt-8 flex flex-col sm:flex-row gap-4 justify-center items-center w-full max-w-lg mx-auto">
           <Button
             message=""
             variant="min-h-10 w-[60vw] sm:flex-1 bg-white hover:bg-rajutPeach text-rajutBoldPink rounded-full font-poppins"
           >
-            Subscribe to Newsletter
+            Berlangganan NewsLetter
           </Button>
         </div>
       </div>
